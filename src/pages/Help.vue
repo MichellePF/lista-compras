745b24de0fa3ce722b2d01b39cf64457bd0d7cd8
Michelle PF <michellepadillafranco@gmail.com>
2020-08-17 12:38:29 -0400

    lista de compras con quasar

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center q-px-md" >
+    <h3 class="text-h3">Lista de Compras</h3>
+    <p class="text-body2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nihil ullam fugiat blanditiis expedita iusto dolorum officia, qui accusantium quod esse earum eligendi ex dolore ut corrupti laudantium eum rem!</p>
+    <p class="text-body2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nihil ullam fugiat blanditiis expedita iusto dolorum officia, qui accusantium quod esse earum eligendi ex dolore ut corrupti laudantium eum rem!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Help'
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <!-- Lista de Items -->
+    <q-list class="bg-white q-pa-md" separator bordered>
+      <q-item v-for="(item, i) in items" :key="i" tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="item.listo" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{item.texto}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      items: [
+        {
+          texto: 'Cloro',
+          listo: false
+        },
+        {
+          texto: 'Pan batido',
+          listo: false
+        },
+        {
+          texto: 'Chocolate',
+          listo: false
+        }
+      ]
+    }
+  }
+}
+</script>
